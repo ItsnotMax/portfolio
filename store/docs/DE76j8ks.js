60876574ba5d9a6bfec8b55faf10a6660b6b2b2a
ItsnotMax <makc.9393@mail.ru>
2025-04-02 12:29:08 +0300

    store deploy fix

---
+++ store/docs/DE76j8ks.js
@@ -0,0 +1,1 @@
+import{b as a,o as e,t as r}from"./MNmVBGFI.js";const s={__name:"ProductDescription",props:{data:{type:String}},setup(t){return(o,n)=>(e(),a("div",null,r(t.data),1))}};export{s as default};
