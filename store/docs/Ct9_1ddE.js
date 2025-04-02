a6c61f351af3a22cd97028a4412124f86772ac74
ItsnotMax <makc.9393@mail.ru>
2025-04-02 12:42:40 +0300

    store deploy fix

---
+++ store/docs/Ct9_1ddE.js
@@ -0,0 +1,1 @@
+import{b as a,o as e,t as r}from"./u-qqrFj2.js";const s={__name:"ProductDescription",props:{data:{type:String}},setup(t){return(o,n)=>(e(),a("div",null,r(t.data),1))}};export{s as default};
---
+++ store/docs/Cv172Awy.js
@@ -0,0 +1,1 @@
+import{b as a,o as e,t as r}from"./u-qqrFj2.js";const s={__name:"ProductSpecs",props:{data:{type:String}},setup(t){return(o,c)=>(e(),a("div",null,r(t.data),1))}};export{s as default};
